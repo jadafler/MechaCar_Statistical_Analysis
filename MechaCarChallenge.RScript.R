3aad5494df7e835764001cffad22e21f8aae448f
jadafler <71464211+jadafler@users.noreply.github.com>
2021-04-17 16:53:29 -0500

    Deliverable 2

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -1,7 +1,7 @@
 # Load library
 library(dplyr)
 
-# Read in CSV
+# Read in MechaCar_mpg CSV
 MechaCar_df <- read.csv('MechaCar_mpg.csv')
 
 # Create linear regression module
@@ -9,3 +9,12 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 # Create summary
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, MechaCar_df))
+
+# Read in Suspension_Coil CSV
+Suspension_df <- read.csv('Suspension_Coil.csv')
+
+# Create Total_Summary_df
+Total_Summary_df <- Suspension_df %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+
+# Create Lot_Summary_df
+Lot_Summary_df <- Suspension_df %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep')
--- README.md
+++ README.md
@@ -6,5 +6,6 @@ Module 15
 <li> The variables/coefficients that provide a non-random amount of variance to the MPG value are: Vehicle Weight, Spoiler Angle, and AWD feature. </li>
 <li> The p-value of the linear regression analysis is 5.35e-11 which is below the assumed significance level of 0.05%; the slope of the linear model is not considered to be zero. </li>
 <li> The model does effectively predict the MPG as the r-squared value is .71 which means that 70% of the predictions will be correct using the linear model. </li>
-
+<br>
+<br>
 <img src="Images/Summary.png" alt="MPG LM Summary">
